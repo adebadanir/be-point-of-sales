1fef974a9f386075d0fc73d6f93b1d41aedf1db1
andrew-jubelio <royce.andrew@jubelio.com>
2025-09-28 09:34:08 +0700

    chore: change to supabase for development

---
+++ prisma/migrations/20250928023303_init/migration.sql
@@ -0,0 +1,108 @@
+-- CreateTable
+CREATE TABLE "public"."admins" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(32) NOT NULL,
+    "email" VARCHAR(64) NOT NULL,
+    "phone_number" VARCHAR(16) NOT NULL,
+    "password" VARCHAR(16) NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."products" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(64) NOT NULL,
+    "price" INTEGER NOT NULL,
+    "category" VARCHAR(32) NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "image" VARCHAR(255) NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."orders" (
+    "id" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "total_price" INTEGER NOT NULL,
+    "status" VARCHAR(32) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "invoice_id" TEXT,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."order_items" (
+    "id" TEXT NOT NULL,
+    "order_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."payments" (
+    "id" TEXT NOT NULL,
+    "order_id" TEXT NOT NULL,
+    "payment_method" VARCHAR(32) NOT NULL,
+    "total_price" INTEGER NOT NULL,
+    "status" VARCHAR(32) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."invoices" (
+    "id" TEXT NOT NULL,
+    "order_id" TEXT NOT NULL,
+    "payments_id" TEXT NOT NULL,
+    "status" VARCHAR(32) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "invoices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_email_key" ON "public"."admins"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_phone_number_key" ON "public"."admins"("phone_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_order_id_key" ON "public"."payments"("order_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "invoices_order_id_key" ON "public"."invoices"("order_id");
+
+-- AddForeignKey
+ALTER TABLE "public"."orders" ADD CONSTRAINT "orders_invoice_id_fkey" FOREIGN KEY ("invoice_id") REFERENCES "public"."invoices"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "public"."orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."order_items" ADD CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."payments" ADD CONSTRAINT "payments_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "public"."orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
